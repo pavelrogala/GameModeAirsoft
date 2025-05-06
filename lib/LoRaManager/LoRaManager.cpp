ae44d39480ceacb760bd0b9b36e82d1ca8626365
Pavel Rogala <pavel.rogala@gmail.com>
2025-05-06 02:19:10 +0200

    Added LoRa manager

--- lib/LoRaManager/LoRaManager.cpp
+++ lib/LoRaManager/LoRaManager.cpp
@@ -1,1 +1,45 @@
-// LoRaManager class definition
+#include "LoRaManager.h"
+#include <SPI.h>
+#include <LoRa.h>
+
+#define LORA_SS 18
+#define LORA_RST 14
+#define LORA_DIO0 26
+
+LoRaManager::LoRaManager(long frequency)
+    : _frequency(frequency) {}
+
+bool LoRaManager::begin() {
+    LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+
+    if (!LoRa.begin(_frequency)) {
+        Serial.println("LoRa init failed. Check your connections.");
+        return false;
+    }
+
+    Serial.println("LoRa init succeeded.");
+    return true;
+}
+
+void LoRaManager::sendMessage(const String& message) {
+    LoRa.beginPacket();
+    LoRa.print(message);
+    LoRa.endPacket();
+}
+
+bool LoRaManager::receiveMessage(String& outMessage) {
+    int packetSize = LoRa.parsePacket();
+    if (packetSize == 0) {
+        return false;
+    }
+
+    outMessage = "";
+    while (LoRa.available()) {
+        outMessage += (char)LoRa.read();
+    }
+    return true;
+}
+
+int LoRaManager::packetRssi() const {
+    return LoRa.packetRssi();
+}
--- lib/LoRaManager/LoRaManager.h
+++ lib/LoRaManager/LoRaManager.h
@@ -1,1 +1,18 @@
-// LoRaManager class declaration
+#ifndef LORA_MANAGER_H
+#define LORA_MANAGER_H
+
+#include <Arduino.h>
+
+class LoRaManager {
+public:
+    LoRaManager(long frequency = 915E6);  // Default to 915 MHz
+    bool begin();
+    void sendMessage(const String& message);
+    bool receiveMessage(String& outMessage);
+    int packetRssi() const;
+
+private:
+    long _frequency;
+};
+
+#endif
